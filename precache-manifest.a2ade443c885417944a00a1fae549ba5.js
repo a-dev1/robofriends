d50913b642c44a1c69bc65e62e26400a02af38ad
code-shoot <franksmithattheshoot@gmail.com>
2020-08-11 08:35:38 +0530

    Updates

---
+++ precache-manifest.a2ade443c885417944a00a1fae549ba5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a265fe777bedba0433671585e9a87a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "043da66a5ebeb4378f44",
+    "url": "/robofriends/static/css/main.c12b4417.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043da66a5ebeb4378f44",
+    "url": "/robofriends/static/js/main.f263d88d.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
